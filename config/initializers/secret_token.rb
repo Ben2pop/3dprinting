90c21b1aa780517ac35f6dc048dd68294d14c81e
Raphael Bendenoun <r.bendenoun@gmail.com>
2013-08-28 14:22:22 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Printing3d::Application.config.secret_token = 'd554695f38fec6c9a5b1516cbccff2b72ed823d60865e2fa861cd12f05d43f8e20df2fbff6aa6d0083c5192360c889dfc5baa367900ef1dd9bd66f5c32710c5b'
